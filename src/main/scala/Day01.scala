b11f2de5424ee2d28403a062933190435c9f4e79
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2020-12-02 02:07:29 +0300

    Rename solve functions

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -9,16 +9,16 @@ object Day01:
       .combinations(n)
       .collectFirst { case xs if xs.sum == 2020 => xs.reduce(_ combine _) }
 
-  def solve01A(xs: List[Int]): Option[Int] =
+  def solveA(xs: List[Int]): Option[Int] =
     solve(xs, 2)
 
-  def solve01B(xs: List[Int]): Option[Int] =
+  def solveB(xs: List[Int]): Option[Int] =
     solve(xs, 3)
 
   def main(args: Array[String]): Unit =
     val i1 = file.readAll("data/1a.txt").map(_.toInt)
     val i2 = file.readAll("data/1b.txt").map(_.toInt)
-    val r1 = solve01A(i1)
-    val r2 = solve01B(i2)
+    val r1 = solveA(i1)
+    val r2 = solveB(i2)
     println(r1) // 1020099
     println(r2) // 49214880
--- src/test/scala/Day01Suite.scala
+++ src/test/scala/Day01Suite.scala
@@ -4,5 +4,5 @@ import org.junit.Assert._
 class Day01Suite:
   @Test def check(): Unit =
     val input = List(1721, 979, 366, 299, 675, 1456)
-    val result = Day01.solve01A(input)
+    val result = Day01.solveA(input)
     assertEquals(result, Some(514579))
