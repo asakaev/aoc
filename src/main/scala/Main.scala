76fb8183c5d0522c597f21ceb48b2b7594dbd25f
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2020-12-01 13:46:44 +0300

    Add 1b

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,19 +4,28 @@ object Main {
   def data(file: String): List[String] =
     scala.io.Source.fromFile(file).getLines.toList
 
-  def expense(xs: List[Int]): Int =
+  def part1(xs: List[Int]): Int =
     xs
       .combinations(2)
       .collect { case xs @ a :: b :: Nil if (a + b == 2020) => xs }
       .flatten
       .foldLeft(1)(_ * _)
 
-  def main(args: Array[String]): Unit = {
-    val input = data("data/1a.txt")
-    val xs = input.map(_.toInt)
+  def part2(xs: List[Int]): Int =
+    xs
+      .combinations(3)
+      .collect { case xs @ a :: b :: c :: Nil if (a + b + c == 2020) => xs }
+      .flatten
+      .foldLeft(1)(_ * _)
 
-    val result1 = expense(xs)
-    println(result1)
+  def main(args: Array[String]): Unit = {
+    val i1 = data("data/1a.txt").map(_.toInt)
+    val i2 = data("data/1b.txt").map(_.toInt)
+
+    val r1 = part1(i1)
+    val r2 = part2(i2)
+    println(r1) // 1020099
+    println(r2) // 49214880
   }
 
 }
--- src/test/scala/Test1.scala
+++ src/test/scala/Test1.scala
@@ -4,7 +4,7 @@ import org.junit.Assert._
 class Test1 {
   @Test def t1(): Unit = {
     val input = List(1721, 979, 366, 299, 675, 1456)
-    val result = Main.expense(input)
+    val result = Main.part1(input)
     assertEquals(result, 514579)
   }
 }
