5209f4207a07bcd7f2117a99d4446b9c70d259ff
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2020-12-07 05:54:52 +0300

    Add 4b

--- src/main/scala/Day04.scala
+++ src/main/scala/Day04.scala
@@ -55,13 +55,69 @@ object Day04:
       xs.foldLeft(Map.empty[Tag, String])((acc, s) => passport(s) ++ acc )
     }
 
-  def validPassportA(p: Passport): Boolean =
+  def validPassportFields(p: Passport): Boolean =
     (p.keys.toSet - CountryId).size == 7
 
+  object Validation:
+    val HexColorCode = """^#([a-fA-F0-9]{6})$""".r
+    val Digits9 = """^[0-9]{9}$""".r
+
+    def validBirthYear(s: String): Boolean =
+      val v = s.toInt
+      1920 <= v && v <= 2002
+
+    def validIssueYear(s: String): Boolean =
+      val v = s.toInt
+      2010 <= v && v <= 2020
+
+    def validExpirationYear(s: String): Boolean =
+      val v = s.toInt
+      2020 <= v && v <= 2030
+
+    def validHeight(s: String): Boolean =
+      val Cm = "cm"
+      val In = "in"
+      s match {
+        case s if s.endsWith(Cm) =>
+          val v = s.split(Cm)(0).toInt
+          150 <= v && v <= 193
+        case s if s.endsWith(In) =>
+          val v = s.split(In)(0).toInt
+          59 <= v && v <= 76
+        case _ => false
+      }
+
+    def validHairColor(s: String): Boolean =
+      HexColorCode.matches(s)
+
+    def validEyeColor(s: String): Boolean =
+      Set("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(s)
+
+    def validPasportId(s: String): Boolean =
+      Digits9.matches(s)
+
+  def validPassportData(p: Passport): Boolean =
+    p.forall {
+      case (BirthYear, s)      => Validation.validBirthYear(s)
+      case (BirthYear, s)      => Validation.validBirthYear(s)
+      case (IssueYear, s)      => Validation.validIssueYear(s)
+      case (ExpirationYear, s) => Validation.validExpirationYear(s)
+      case (Height, s)         => Validation.validHeight(s)
+      case (HairColor, s)      => Validation.validHairColor(s)
+      case (EyeColor, s)       => Validation.validEyeColor(s)
+      case (PassportId, s)     => Validation.validPasportId(s)
+      case (CountryId, _)      => true
+    }
+
   def solveA(xs: List[String]): Int =
-    passports(xs).count(validPassportA)
+    passports(xs).count(validPassportFields)
+
+  def solveB(xs: List[String]): Int =
+    passports(xs).count(p => validPassportFields(p) && validPassportData(p))
 
   def main(args: Array[String]): Unit =
     val i = file.readAll("data/4a.txt")
     val r1 = solveA(i)
+    val r2 = solveB(i)
     println(r1) // 245
+    println(r2) // 133
--- src/test/scala/Day04Suite.scala
+++ src/test/scala/Day04Suite.scala
@@ -26,13 +26,27 @@ class Day04Suite:
     )
     assertEquals(passports(xs), expected)
 
-  @Test def checkValidPassport(): Unit =
+  @Test def checkValidPassportFields(): Unit =
     val p1 = Passport(Map(CountryId -> "147", PassportId -> "860033327", Height -> "183cm", ExpirationYear -> "2020", IssueYear -> "2017", HairColor -> "#fffffd", BirthYear -> "1937", EyeColor -> "gry"))
     val p2 = Passport(Map(CountryId -> "350", PassportId -> "028048884", ExpirationYear -> "2023", IssueYear -> "2013", HairColor -> "#cfa07d", BirthYear -> "1929", EyeColor -> "amb"))
     val p3 = Passport(Map(PassportId -> "760753108", Height -> "179cm", ExpirationYear -> "2024", IssueYear -> "2013", HairColor -> "#ae17e1", BirthYear -> "1931", EyeColor -> "brn"))
     val p4 = Passport(Map(PassportId -> "166559648", EyeColor -> "brn", Height -> "59in", ExpirationYear -> "2025", IssueYear -> "2011", HairColor -> "#cfa07d"))
+    assertEquals(validPassportFields(p1), true)
+    assertEquals(validPassportFields(p2), false)
+    assertEquals(validPassportFields(p3), true)
+    assertEquals(validPassportFields(p4), false)
 
-    assertEquals(validPassportA(p1), true)
-    assertEquals(validPassportA(p2), false)
-    assertEquals(validPassportA(p3), true)
-    assertEquals(validPassportA(p4), false)
+  @Test def checkValidHeight(): Unit =
+    assertEquals(Validation.validHeight("60in"), true)
+    assertEquals(Validation.validHeight("190cm"), true)
+    assertEquals(Validation.validHeight("190in"), false)
+    assertEquals(Validation.validHeight("190"), false)
+
+  @Test def checkValidHairColor(): Unit =
+    assertEquals(Validation.validHairColor("#123abc"), true)
+    assertEquals(Validation.validHairColor("#123abz"), false)
+    assertEquals(Validation.validHairColor("123abc"), false)
+
+  @Test def checkValidPasportId(): Unit =
+    assertEquals(Validation.validPasportId("000000001"), true)
+    assertEquals(Validation.validPasportId("0123456789"), false)
